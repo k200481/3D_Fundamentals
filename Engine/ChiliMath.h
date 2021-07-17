8c03863dce055b727a91c36f77d5fc77ffb19929
Sarim <msaarimkh@gmail.com>
2021-07-17 10:26:46 +0500

    wrap_angle

--- Engine/ChiliMath.h
+++ Engine/ChiliMath.h
@@ -23,9 +23,17 @@
 #include <math.h>
 
 constexpr float PI = 3.14159265f;
+constexpr double PI_D = 3.1415926535897932;
 
 template <typename T>
 inline auto sq( const T& x )
 {
 	return x * x;
+}
+
+template <typename T>
+inline auto wrap_angle( T angle )
+{
+	T moddedAngle = (T)fmod( angle, (T)2.0 * (T)PI_D );
+	return ( moddedAngle > (T)PI_D ) ? ( moddedAngle - (T)2.0 * (T)PI_D ) : moddedAngle;
 }
