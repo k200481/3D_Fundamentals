0ff4223f644de70eccb128ab49106b06587331f2
Sarim <msaarimkh@gmail.com>
2021-07-17 20:41:52 +0500

    Cube returns indexed triange list

--- Engine/Cube.h
+++ Engine/Cube.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include "Vec3.h"
 #include "IndexedLineList.h"
+#include "IndexedTriangleList.h"
 
 class Cube
 {
@@ -28,6 +29,18 @@ public:
 				0,1 , 1,2 , 2,3 , 3,0,
 				0,4 , 1,5 , 2,6 , 3,7,
 				4,5 , 5,6 , 6,7 , 7,4
+			}
+		};
+	}
+	IndexedTriangleList GetTriangles() const
+	{
+		return { vertices, {
+				0,1,2 , 2,3,0,
+				4,5,6 , 6,7,4,
+				0,3,7 , 7,4,0,
+				1,2,5 , 2,5,6,
+				0,1,4 , 1,4,5,
+				3,2,7 , 2,7,6
 			}
 		};
 	}
