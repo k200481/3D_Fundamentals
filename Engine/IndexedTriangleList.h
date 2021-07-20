5012d2888451aea43ea9e0060065629883ed5062
Sarim <msaarimkh@gmail.com>
2021-07-20 14:26:21 +0500

    vector of culling flags

--- Engine/IndexedTriangleList.h
+++ Engine/IndexedTriangleList.h
@@ -5,6 +5,17 @@
 
 struct IndexedTriangleList
 {
+	IndexedTriangleList( std::vector<Vec3> vertices_in, std::vector<size_t> indices_in )
+		:
+		vertices( std::move(vertices_in) ),
+		indices( std::move(indices_in) )
+	{
+		assert( vertices.size() > 2 );
+		assert( indices.size() % 3 == 0 );
+		cullingFlags.resize( indices.size() / 3, false );
+	}
+
 	std::vector<Vec3> vertices;
 	std::vector<size_t> indices;
+	std::vector<bool> cullingFlags;
 };
