7981ec9bc91cb75b6cc7ecbe493ef6b776898d75
Sarim <msaarimkh@gmail.com>
2021-08-11 13:34:06 +0500

    Perspective Correction

--- Engine/CoordinateTransformer.h
+++ Engine/CoordinateTransformer.h
@@ -13,16 +13,23 @@ public:
 	{
 	}
 
-	Vec3& Transform( Vec3& v ) const
+	template <typename Vertex>
+	Vertex& Transform( Vertex& v ) const
 	{
-		const float zInv = 1.0f / v.z;
-		v.x = ( (  v.x * zInv  ) + 1.0f ) * xFactor;
-		v.y = ( ( -v.y * zInv ) + 1.0f ) * yFactor;
+		const float zInv = 1.0f / v.pos.z;
+
+		v *= zInv;
+		v.pos.x = ( v.pos.x + 1.0f ) * xFactor;
+		v.pos.y = ( -v.pos.y + 1.0f ) * yFactor;
+
+		v.pos.z = zInv;
+
 		return v;
 	}
-	Vec3 GetTransformed( const Vec3& v ) const
+	template <typename Vertex>
+	Vertex GetTransformed( const Vertex& v ) const
 	{
-		return Transform( Vec3(v) );
+		return Transform( Vertex(v) );
 	}
 
 private:
--- Engine/Pipeline.h
+++ Engine/Pipeline.h
@@ -31,6 +31,7 @@ public:
 		translation = translation_in;
 	}
 private:
+	// transforms vertices to world space
 	void ProcessVertices(const std::vector<Vertex>& vertices, const std::vector<size_t>& indices)
 	{
 		// create vertex vector for vs output
@@ -45,6 +46,8 @@ private:
 		// assemble triangles from stream of indices and vertices
 		AssembleTriangles(verticesOut, indices);
 	}
+	// creates triangles from the vertices and indices
+	// also culls backfaces
 	void AssembleTriangles(const std::vector<Vertex>& vertices, const std::vector<size_t>& indices)
 	{
 		// assemble triangles in the stream and process
@@ -63,20 +66,23 @@ private:
 			}
 		}
 	}
+	// creates a triangle object
 	void ProcessTriangle(const Vertex& v0, const Vertex& v1, const Vertex& v2)
 	{
 		PostProcessTriangleVertices(Triangle<Vertex>{ v0, v1, v2 });
 	}
+	// transforms triangle's vertices to perspective/screen space
 	void PostProcessTriangleVertices(Triangle<Vertex>& triangle)
 	{
-		ct.Transform(triangle.v0.pos);
-		ct.Transform(triangle.v1.pos);
-		ct.Transform(triangle.v2.pos);
+		ct.Transform( triangle.v0 );
+		ct.Transform( triangle.v1 );
+		ct.Transform( triangle.v2 );
 
 		// draw the triangle
 		DrawTriangle(triangle);
 	}
-
+	// identifies the type of triangle and calls either draw flat top or draw flat bottom
+	// with the appropriate parameters
 	void DrawTriangle(const Triangle<Vertex>& triangle)
 	{
 		// using pointers so we can swap (for sorting purposes)
@@ -123,35 +129,46 @@ private:
 			}
 		}
 	}
+	// does the parts unique to flat top triangles and calls draw flat triangle 
 	void DrawFlatTopTriangle(const Vertex& it0, const Vertex& it1, const Vertex& it2)
 	{
+		// total y distance
 		const float dy = it2.pos.y - it0.pos.y;
+		// change in left edge for every change in y
 		const auto di0 = ( it2 - it0 ) / dy;
+		// change in right edge for every change in y
 		const auto di1 = ( it2 - it1 ) / dy;
 
+		// right edge, starts at v1 in this case
 		const auto itEdge1 = it1;
 
 		DrawFlatTriangle( it0, it1, it2, di0, di1, itEdge1 );
 	}
-
+	// does the parts unique to flat bottom tirangles and calls draw flat triangle
 	void DrawFlatBottomTriangle(const Vertex& it0, const Vertex& it1, const Vertex& it2)
 	{
+		// total y distance
 		const float dy = it2.pos.y - it0.pos.y;
+		// change in left edge for every change in y
 		const auto di0 = ( it1 - it0 ) / dy;
+		// change in rught edge for every change in y
 		const auto di1 = ( it2 - it0 ) / dy;
 
+		// right edge, starts at v0 in this case
 		const auto itEdge1 = it0;
 
 		DrawFlatTriangle( it0, it1, it2, di0, di1, itEdge1 );
 	}
-	
+	// draws a flat triangle, handles the parts in common in both flat top and flat bottom triangles
 	void DrawFlatTriangle(const Vertex& it0, const Vertex& it1, const Vertex& it2, const Vertex& dv0, const Vertex& dv1, Vertex itEdge1)
 	{
 		const int yStart = (int)std::ceil( it0.pos.y - 0.5f );
 		const int yEnd = (int)std::ceil( it2.pos.y - 0.5f );
 
+		// left edge, alwas v0
 		auto itEdge0 = it0;
 
+		// pre steps
 		itEdge0 += dv0 * ( float(yStart) + 0.5f - it0.pos.y );
 		itEdge1 += dv1 * ( float(yStart) + 0.5f - it0.pos.y );
 
@@ -161,13 +178,21 @@ private:
 			const int xStart = (int)std::ceil(itEdge0.pos.x - 0.5f);
 			const int xEnd = (int)std::ceil(itEdge1.pos.x - 0.5f);
 
+			// the coordinate in perspective space to be drawn
 			Vertex tc = itEdge0;
+			// the rate at which this coordinate changes with every change in x
 			Vertex dt = ( itEdge1 - itEdge0 ) / ( itEdge1.pos.x - itEdge0.pos.x );
 			for ( int x = xStart; x < xEnd; x++, tc += dt )
 			{
+				// z actually stores zInv
+				const float z = 1.0f / tc.pos.z;
+				// recover the coordinate in object space 
+				// (technically world space since they are trandformed and rotated)
+				// can't use tc here because it is being added into
+				const auto v = tc * z;
 				gfx.PutPixel(
 					x, y,
-					effect.ps( tc )
+					effect.ps( v )
 				);
 			}
 		}
