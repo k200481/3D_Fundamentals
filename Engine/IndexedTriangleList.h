6a65fcaf22977bd4b85bf7a45d43e801b5cebd46
Sarim <msaarimkh@gmail.com>
2021-08-14 18:20:14 +0500

    Loading objects with normals

--- Engine/IndexedTriangleList.h
+++ Engine/IndexedTriangleList.h
@@ -101,6 +101,94 @@ struct IndexedTriangleList
 		}
 		return itlist;
 	}
+	// load indexed triangle list containing normals from an object file
+	static IndexedTriangleList<T> LoadNormals( const std::string& filename )
+	{
+		IndexedTriangleList<T> itlist;
+
+		// check if vertices are arranged clockwise
+		bool isCCW = false;
+		{
+			std::ifstream file;
+			std::string line;
+			std::getline( file, line );
+			std::transform( line.begin(), line.end(), line.begin(), std::tolower );
+			if ( line.find("ccw") != std::string::npos )
+			{
+				isCCW = true;
+			}
+		}
+
+		// resources fille by the loader
+		tinyobj::attrib_t attrib;
+		std::vector<tinyobj::shape_t> shapes;
+		std::string err;
+
+		// load obj file and save return val
+		const bool returnVal = tinyobj::LoadObj( &attrib, &shapes, nullptr, &err, filename.c_str() );
+
+		// check for errors
+		if ( !err.empty() && err.substr(0, 4) == "WARN" )
+		{
+			throw std::runtime_error( "LoadObj returned error: " + err + "\nFile: " + filename.c_str() );
+		}
+		else if ( !returnVal )
+		{
+			throw std::runtime_error( "LoadObj returned false\nFile: " + filename );
+		}
+		else if ( shapes.size() == 0u )
+		{
+			throw std::runtime_error( "File " + filename + " contains no shapes" );
+		}
+
+		// extract vertices
+		itlist.vertices.reserve( attrib.vertices.size() / 3u );
+		for ( size_t i = 0; i < attrib.vertices.size(); i += 3 )
+		{
+			itlist.vertices.emplace_back( Vec3{
+				attrib.vertices[i],
+				attrib.vertices[i + 1],
+				attrib.vertices[i + 2]
+			} );
+		}
+
+		// extract indices
+		const auto& mesh = shapes[0].mesh;
+
+		itlist.indices.reserve( mesh.indices.size() );
+		// add one face at a time
+		for ( size_t f = 0; f < mesh.num_face_vertices.size(); f++  )
+		{
+			// check if face has the right number of faces
+			if ( mesh.num_face_vertices[f] != 3u )
+			{
+				std::ostringstream oss;
+				oss << "Face #" << f << " has " << mesh.num_face_vertices[f] << " faces";
+				throw std::runtime_error( oss.str().c_str() );
+			}
+			// add indices and normals
+			for ( size_t i = 0; i < 3u; i++ )
+			{
+				const auto idx = mesh.indices[f * 3u + i];
+				itlist.indices.push_back( size_t( idx.vertex_index ) );
+
+				itlist.vertices[idx.vertex_index].n = Vec3{
+					attrib.normals[idx.normal_index + 0],
+					attrib.normals[idx.normal_index + 1],
+					attrib.normals[idx.normal_index + 2]
+				};
+			}
+
+			if ( isCCW )
+			{
+				// correct the order
+				std::swap( itlist.indices.back(), *std::prev( itlist.indices.end(), 2 ) );
+			}
+
+		}
+		return itlist;
+	}
+	
 	// 
 	void AdjustToTrueCenter()
 	{
